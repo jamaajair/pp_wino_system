06406d47e7c22360d2bd1a92d451309ff9265dd5
Jamaa Jair <jamaajair98@gmail.com>
2026-02-16 21:39:31 +0100

    first step

---
+++ backend/src/main/java/com/wino/demo/WinoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wino.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinoBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wino/demo/WinoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wino.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
